2d0d3a38cc88d246c59369a299e4e6ac92d9f2de
Maharjan Anish <dangol10@hotmail.com>
2015-04-07 10:06:11 +0545

    huge bug fixes

--- app.js
+++ app.js
@@ -1,25 +1,24 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
-http.listen(3000, function(){
-  console.log('listening on *:3000');
-});
-
-/*var express = require('express'),
+var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server);
 
-server.listen(3000);
-*/
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+server.listen(3000, function(){
+	console.log('listen on 3000');
+});
+
+app.get('/',function(req,res){
+	res.sendFile(__dirname + '/index.html');
 });
 
 io.sockets.on('connection',function(socket){
 	socket.on('send message',function(data){
 		io.sockets.emit('new message',data);
-		/*socket.broadcast.emit('new message',data);*/
+	});
+
+	console.log('user connected');
+	socket.on('disconnect',function(){
+		console.log('user disconneced');
 	});
 });
+
