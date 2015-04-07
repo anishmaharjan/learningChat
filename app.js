72d12991a25a43b1c9545efccee91ae84bc673bd
Maharjan Anish <dangol10@hotmail.com>
2015-04-07 12:58:53 +0545

    added username feature

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io').listen(server),
+	nicknames = [];
 
 server.listen(3000, function(){
 	console.log('listen on 3000');
@@ -12,13 +13,35 @@ app.get('/',function(req,res){
 });
 
 io.sockets.on('connection',function(socket){
+	socket.on('new user', function(data, callback){
+		if(nicknames.indexOf(data) != -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.nickname = data; 
+			nicknames.push(socket.nickname);
+			console.log('with name = ' +socket.nickname);
+			/*io.sockets.emit('usernames',nicknames);*/
+			updateNicknames();
+		}
+	});
+
+	function updateNicknames(){
+		io.sockets.emit('usernames', nicknames);
+	}
+
 	socket.on('send message',function(data){
-		io.sockets.emit('new message',data);
+		io.sockets.emit('new message',{msg: data, nick: socket.nickname});
 	});
 
 	console.log('user connected');
-	socket.on('disconnect',function(){
+	socket.on('disconnect',function(data){
+		if(!socket.nickname) return;
+		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		updateNicknames();
 		console.log('user disconneced');
 	});
+
+
 });
 
