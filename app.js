190f3b7fab59061f9586e2084370b95754047962
Maharjan Anish <dangol10@hotmail.com>
2015-04-09 18:18:00 +0545

    added private messaging

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
 	io = require('socket.io').listen(server),
-	nicknames = [];
+	users = {};
 
 server.listen(3000, function(){
 	console.log('listen on 3000');
@@ -14,12 +14,13 @@ app.get('/',function(req,res){
 
 io.sockets.on('connection',function(socket){
 	socket.on('new user', function(data, callback){
-		if(nicknames.indexOf(data) != -1){
+		if(data in users){
 			callback(false);
 		}else{
 			callback(true);
 			socket.nickname = data; 
-			nicknames.push(socket.nickname);
+			users[socket.nickname]  = socket;
+			/*nicknames.push(socket.nickname);*/
 			console.log('with name = ' +socket.nickname);
 			/*io.sockets.emit('usernames',nicknames);*/
 			updateNicknames();
@@ -27,17 +28,36 @@ io.sockets.on('connection',function(socket){
 	});
 
 	function updateNicknames(){
-		io.sockets.emit('usernames', nicknames);
+		io.sockets.emit('usernames', Object.keys(users));
 	}
 
-	socket.on('send message',function(data){
-		io.sockets.emit('new message',{msg: data, nick: socket.nickname});
+	socket.on('send message',function(data, callback){
+		var msg = data.trim();
+		if(msg.substr(0,3) === '/w '){
+			msg = msg.substr(3);
+			var ind = msg.indexOf(' ');
+			if(ind !== -1){
+				var name = msg.substring(0, ind);
+				var msg = msg.substring(ind+1);
+				if(name in users){
+					users[name].emit('whisper',{msg: msg, nick: socket.nickname})
+					console.log('wisper');
+				}else{
+					callback('Error: Enter a valid user');
+				}
+			}else{
+				callback('Error please enter message.');
+			}
+		}else{
+			io.sockets.emit('new message',{msg: msg, nick: socket.nickname});
+		}
 	});
 
 	console.log('user connected');
 	socket.on('disconnect',function(data){
 		if(!socket.nickname) return;
-		nicknames.splice(nicknames.indexOf(socket.nickname), 1);
+		delete users[socket.nickname];
+		/*nicknames.splice(nicknames.indexOf(socket.nickname), 1);*/
 		updateNicknames();
 		console.log('user disconneced');
 	});
